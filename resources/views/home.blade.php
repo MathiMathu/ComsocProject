f9beec6738d00faa7fd917455c1d55667ab9456b
tharindu45 <prasadtharindu816@gmail.com>
2020-11-26 02:10:22 -0800

    Create frontend template

---
+++ resources/views/home.blade.php
@@ -0,0 +1,172 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home page</title>
+    <link rel = "stylesheet" type = "text/css" href = "css/bootstrap.min.css">
+	<link rel = "stylesheet" type = "text/css" href = "css/footer.css">
+	<script type="text/javascript" src="https://gc.kis.v2.scr.kaspersky-labs.com/FD126C42-EBFA-4E12-B309-BB3FDD723AC1/main.js?attr=v6HePCTnT6Z0HoRqAnrN84RI9LNaEztjACmbi70896NcVWOwpUEAV75w22zaJwFoGrU8UT_8Mu83kcORnkhBTK3YIIza3IvA4wicuw85IsMDzi7g_eBCL2M8sLlyv3B_puWdyO8Q6IjIxudA0qVwp6TPiAslUFASojXN3XnpgcDUIEjqJR759_IoCTZIMvSMvFUBHSbvj7ZRfvOEQhLiH0PTnvBd3w5hZsOu7EuaCjosZohatxu9mpHL96Dq6tbctI7hnUKs864AfPb08onoqdK7SvJTKdOd8YqttHzAQn-ksn5-VatlBQAnDtO-LPXmyiWNge7Xub1Vrj6swdAOsGbP3wtecStM0erT0py1lka_p9XdX4pumayu05MATm3MevONzOOLxRVQQLngZBStCxmDKww08DWuYxugAWOpSKR5T5qh6Od-nGbmhu08V08Aj-XObNTOo9JvsFJjMiZpRA" charset="UTF-8"></script></head>
+	
+    
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    <style>
+          .dropdown-menu a:hover {
+          color: #1608d4 !important;
+         }
+         a:hover {
+            color:#1608d4 !important; 
+         }
+         .nav-item{
+             margin-left:10px;
+         }
+         .nav-item1{
+            margin-left:2px; 
+         }
+
+   
+   </style>
+</head>
+<body>
+    <!--navigation bar-->
+    <nav class="navbar navbar-expand-md navbar-light  sticky-top" style="background-color:#080624; color:#ffffff;">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#"><img src="img/p1.png" alt=""></a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+           
+            <div class="collapse navbar-collapse" id="navbarResponsive">
+                <ul class="navbar-nav  ml-auto">
+                    <li class="nav-item active" >
+                        <a class="nav-link" href="#" style="color:#ffffff;">Home</a>
+                    </li>
+                    <li class="nav-item" >
+                        <a class="nav-link" href="#" style="color:#ffffff;">Karaniyam</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#" style="color:#ffffff;">Event</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#" style="color:#ffffff;">Discussion</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#" style="color:#ffffff;">Financial support</a>
+                    </li>
+                    
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" style="color:#ffffff; a:hoverbackground-color: red;}">About</a>
+                            <div class="dropdown-menu" aria-labelledby="navbarDropdown" style="background-color:#080624;">
+                                <a class="dropdown-item" href="#" style="color:#ffffff;">Out Team</a>
+                                <a class="dropdown-item" href="#" style="color:#ffffff;">Galary</a>
+                            </div>
+                    </li>
+                    <li class="nav-item nav-item1">
+                        <a class="nav-link" href="#" style="color:#ffffff;">Park</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#" class="btn btn-info btn-lg"  style="background-color:#0099cc;">
+                           <span class="glyphicon glyphicon-user"></span> LogIn
+                        </a> 
+                            
+
+                    </li>
+                </ul>
+            </div><!--collapse navibar-collapse-->
+        </div><!--end container-fluid-->
+    </nav><!--end navibar-->
+
+    <!--inside the footer and  navibar-->
+    <div class="container">
+         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores fuga quidem aliquid veritatis quaerat voluptatum unde cumque, 
+         aliquam incidunt deleniti itaque vero tempore obcaecati doloribus ipsam, similique maxime, debitis harum!</p>
+
+
+         <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eligendi porro dignissimos provident cumque doloremque eveniet. Fuga 
+         eligendi, facere deserunt repudiandae atque quod temporibus vel quidem at. Magni ab obcaecati quidem.</p>
+
+         <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eligendi porro dignissimos provident cumque doloremque eveniet. Fuga 
+         eligendi, facere deserunt repudiandae atque quod temporibus vel quidem at. Magni ab obcaecati quidem.</p>
+
+         <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eligendi porro dignissimos provident cumque doloremque eveniet. Fuga 
+         eligendi, facere deserunt repudiandae atque quod temporibus vel quidem at. Magni ab obcaecati quidem.</p>
+
+         <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eligendi porro dignissimos provident cumque doloremque eveniet. Fuga 
+         eligendi, facere deserunt repudiandae atque quod temporibus vel quidem at. Magni ab obcaecati quidem.</p>
+
+         <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eligendi porro dignissimos provident cumque doloremque eveniet. Fuga 
+         eligendi, facere deserunt repudiandae atque quod temporibus vel quidem at. Magni ab obcaecati quidem.</p>
+
+         <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eligendi porro dignissimos provident cumque doloremque eveniet. Fuga 
+         eligendi, facere deserunt repudiandae atque quod temporibus vel quidem at. Magni ab obcaecati quidem.</p>
+
+         <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eligendi porro dignissimos provident cumque doloremque eveniet. Fuga 
+         eligendi, facere deserunt repudiandae atque quod temporibus vel quidem at. Magni ab obcaecati quidem.</p>
+    </div>
+
+
+     <!--footer-->
+     <footer class="site-footer">
+      <div class="container">
+        <div class="row">
+        <div class="col-sm-12 col-md-6">
+            <h6>About</h6>
+            <p class="text-justify">History of Compsoc/ Introduction-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------</p>
+        </div>
+
+            <div class="col-xs-6 col-md-4">
+                <h6 class = "text-center">Contact</h6>
+                <div class="row">
+                    <div class="column left">
+                        <p style = "font-size : 10px"> President,<br>Computer Society (CompSoc),<br>Department of Computer Science,<br>University of Jaffna.<br>Email :  comsoc@univ.jfn.ac.lk<br>Phone : 077 104 4491</p>
+                    </div>
+                    <div class="column right">
+                        <p style = "font-size : 10px">Head,<br>Department of Computer Science,<br>Faculty of Science,<br>University of Jaffna.<br>Email :  dcs@univ.jfn.ac.lk<br>Phone : 021 221 8194</p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-xs-6 col-md-2">
+                <h6>Quick Links</h6>
+                <ul class="footer-links" >
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">About</a></li>
+                    <li><a href="#">Kananiyam</a></li>
+                    <li><a href="#">Events</a></li>
+                    <li><a href="#">Discussion</a></li>
+                    <li><a href="#">CS park</a></li>
+                    <li><a href="#">Financial Support</a></li>
+                </ul>
+            </div>
+        </div>
+        <hr>
+    </div>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-8 col-sm-6 col-xs-12">
+                    <p class="copyright-text">Copyright &copy; 2017 All Rights Reserved by 
+                    <a href="#">Computer Society University of Jaffna </a>.
+                    </p>
+                </div>
+
+                <div class="col-md-4 col-sm-6 col-xs-12">
+                    <ul class="social-icons">
+                    <li><a class="facebook" href="#"><i class="fa fa-facebook"></i></a></li>
+                    <li><a class="twitter" href="#"><i class="fa fa-twitter"></i></a></li>
+                    <li><a class="instagram" href="#"><i class="fa fa-instagram"></i></a></li>
+                    <li><a class="linkedin" href="#"><i class="fa fa-linkedin"></i></a></li>   
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </footer>
+    <!-- end footer class-->
+		
+		<script type = "text/javascript" src = "js/bootstrap.min.js "></script>
+		<script type = "text/javascript" src = "js/Jque.js "></script>
+
+</body>
+</html>
