e164c457f0d43fc841edc7e330d303110509f70b
miahamedaneeque@gmail.com <miahamedaneeque@gmail.com>
2021-01-03 16:13:36 +0530

    DB Models Created

---
+++ app/Models/FinanceSupport.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class financesupport extends Model
+{
+    use HasFactory;
+    public $table= "financesupport";
+}
