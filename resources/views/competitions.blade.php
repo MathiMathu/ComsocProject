6eb41f8f6400a034e48581160cec5fc4a869a2cf
MathiMathu <thusha2024@gmail.com>
2021-06-23 11:56:26 +0530

    Created previous events pages and made some corrections

---
+++ resources/views/competitions.blade.php
@@ -0,0 +1,79 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css"
+        integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/style1.css">
+
+    <title>Competition Details</title>
+</head>
+
+<body style = "background-color: darkblue;">
+
+<div class="row vr-gallery">
+        <div>
+            <div class="row">
+                <div class="col-md-12 col-lg-7 pr-0 pd-md">
+                    <img src="images/winnerslist.jpg" alt="" height="350px">
+                </div>
+                <div class="col-md-12 col-lg-5 light-bg cus-pd cus-arrow-left">
+                    <p><small>18th of April 2020</small></p>
+                    <h3><b>LOCKDOWN Coding Competition - UoJ</b></h3>
+                    <p>
+                    LOCKDOWN Coding Competition - UoJ.
+                    3-Hours Coding Competition held on 18th April 2020, from 6pm to 9pm.
+                    Winners are Janani Kangesan,Denuxshe Chelvaretnem,Deshan Harshana Nawarathna.
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="row vr-gallery">
+        <div>
+            <div class="row">
+                <div class="col-md-12 col-lg-7 pr-0 pd-md">
+                    <img src="images/photography.jpg" alt="" height="900px" width="200px">
+                </div>
+                <div class="col-md-12 col-lg-5 light-bg cus-pd cus-arrow-left">
+                    <p><small>01st of February 2017</small></p>
+                    <h3><b>Photography Competition - UoJ</b></h3>
+                    <p>
+                    Photography Competition held as a celebration of
+                    25th Anniversary of the Department of Computer Science. This Photography Competition was hosted by the Computer Society, Department of Computer science,University of Jaffna <br></br>
+                    Two Categories : <br>
+                    "Nature" <br> "Daily Life" <br>
+                    Deadline was : Feb 1, 2017<br>
+                    * Opened for all students of UoJ
+                    <br>
+                    <b>
+                    "You don't take photographs, You Make it"
+                   
+                    </b>
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+  
+
+    
+    
+
+
+    
+
+
+    <script src="js/jquery-1.12.0.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>>
+    <script src="js/main.js"></script>
+
+</body>
+
+</html>
---
+++ resources/views/techtalk.blade.php
@@ -0,0 +1,107 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css"
+        integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/style1.css">
+
+    <title>Techtalk Details</title>
+</head>
+
+<body style="background-color: darkblue">
+    <div class="row">
+        <div class="col-md-4 pl-4 mb-4">
+            <div class="card">
+                <img class="card-img-top" src="images/techtalk1.jpg" alt="" height="500px">
+                <hr>
+                <div class="card-body bg-gray cus-pd2 cus-arrow-up" style="background-color:grey;">
+                    <p><small>26th of January 2020 <br> from 09.00am</small></p>
+                    <h3><b>TOPIC: </b>Salesforce Session</h3>
+                    <p><b>Resource Persons:</b> Jean Brenda Soudaralingam (Consultant[Salesforce] Virtusa) and S. Thiriyambaga Sarma (Developor[Salesforce] Virtusa)</p>
+                    <br>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-md-4 pl-4 mb-4">
+            <div class="card">
+                <img class="card-img-top" src="images/techtalk2.jpg" alt="" height="500px">
+                <hr>
+                <div class="card-body bg-gray cus-pd2 cus-arrow-up" style="background-color: grey;">
+                    <p><small>25th January 2020 <br> from 09.30am</small></p>
+                    <h3><b>TOPIC: </b>Zero To Hero With GIT & GITHUB </h3>
+                    <p><b>Resource Person:</b> Ahamed Safnaj (GitHub Campus Expert, Trainee Software Engineer at Virtusa)</p>
+                    <br>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-md-4 pl-4 mb-4">
+            <div class="card">
+                <img class="card-img-top" src="images/techtalk3.jpg" alt="" height="500px">
+                <hr>
+                <div class="card-body bg-gray cus-pd2 cus-arrow-up" style="background-color: grey;">
+                    <p><small>24th January 2020 <br> from 10.00am to 11.00am</small></p>
+                    <h3><b>TOPIC: </b>Importance Of Machine Learning In Today's Era</h3>
+                    <p><b>Resource Person:</b> Prof. Ms. S. Padma (Department of Computer Applications, K.S. Rangasamy College of Arts and Science, Tiruchengode,
+                    India)</p>
+                </div>
+            </div>
+        </div>
+
+    </div>
+    <div class="row">
+        <div class="col-md-4 pl-4 mb-4">
+            <div class="card">
+                <img class="card-img-top" src="images/techtalk4.jpg" alt="" height="500px">
+                <hr>
+                <div class="card-body bg-gray cus-pd2 cus-arrow-up" style="background-color:grey;">
+                    <p><small>5th  March 2019 <br> from 11.00am-1.00pm</small></p>
+                    <h3><b>TOPIC: </b>OpenPOWER And IBM AI</h3>
+                    <p><b>Resource Persons:</b> Mr. Ganesan Narayanasamy (OpenPOWER Leader for Academia and research at the IBM Lab) and Mr. Gerard S. Saverimuthu
+                    (Technical Sales Leader, Server Solutions, ASEAN, IBM Systems for Cloud Platforms)</p>
+                    <br>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-md-4 pl-4 mb-4">
+            <div class="card">
+                <img class="card-img-top" src="images/techtalk5.png" alt="" height="500px">
+                <hr>
+                <div class="card-body bg-gray cus-pd2 cus-arrow-up" style="background-color: grey;">
+                    <p><small>15th February 2019 <br> from 9.00am-11.00am</small></p>
+                    <h3><b>TOPIC: </b>Building A Foundation For Digital Transformation</h3>
+                    <p><b>Resource Persons:</b> Mr. Regie Yogendran (Senior Technology Consultant, Nascent Markets, VMware Southeast Asia, VMware, Inc.)</p>
+                    <br><br>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-md-4 pl-4 mb-4">
+            <div class="card">
+                <img class="card-img-top" src="images/techtalk6.jpg" alt="" height="500px">
+                <hr>
+                <div class="card-body bg-gray cus-pd2 cus-arrow-up" style="background-color: grey;">
+                    <p><small>5th February 2019<br> from 9.00am-11.00am</small></p>
+                    <h3><b>TOPIC: </b>NLP Trends And Techniques For Text Analytics</h3>
+                    <p><b>Resource Persons:</b> Dr. A. Ruvan Weerasinghe (Senior Lecturer at the University of Colombo School of Computing [UCSC])</p>
+                    <br>
+                    <br>
+                </div>
+            </div>
+        </div>
+
+    </div>
+    <script src="js/jquery-1.12.0.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/main.js"></script>
+
+</body>
+
+</html>
